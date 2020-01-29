7d8346b077bb5975fbea8c6de6c5125bdd84bf6d
Andres <andrescalderoncl@gmail.com>
2020-01-29 10:36:17 -0500

    Variable de entorno mongo video 113

--- server/config/config.js
+++ server/config/config.js
@@ -15,6 +15,6 @@ let urlDB
 if (process.env.NODE_ENV === "dev") {
     urlDB = "mongodb://localhost:27017/cafe"
 } else {
-    urlDB = "mongodb+srv://root:pcVk8HxU20xsV8Ip@cluster0-1radp.mongodb.net/cafe"
+    urlDB = process.env.MONGO_URL
 }
 process.env.URLDB = urlDB
